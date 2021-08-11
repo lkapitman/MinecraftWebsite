7b7a2a6b0ec73a04417d33f9beecd3514804e0ce
CarryLove <l.kapitman@gmail.com>
2021-08-11 16:28:57 +0300

    создал пакеты. Архитектура всё-таки

--- src/main/java/com/github/lkapitman/controller/HelloController.java
+++ src/main/java/com/github/lkapitman/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.github.lkapitman;
+package com.github.lkapitman.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
