deb402f6ee31e92be0e1c533833fe1ce1286d414
CarryLove <l.kapitman@gmail.com>
2021-08-11 16:27:17 +0300

    Каркас приложения на Spring

---
+++ src/main/java/com/github/lkapitman/HelloController.java
@@ -0,0 +1,13 @@
+package com.github.lkapitman;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+  @GetMapping("/hello")
+  public String hello() {
+    return "hello_world";
+  }
+}
